81e0e8a90155e635bbe765aa048f1361ed0251fa
Valliappan P <34580631+valliappan617@users.noreply.github.com>
2019-02-16 16:22:25 -0600

    Update val.py

--- val.py
+++ val.py
@@ -1,1 +1,17 @@
 print(4+8)
+a = [1, 5, 8]
+b = [2, 6, 9, 10]
+c = [100, 200]
+
+print(max(a))
+
+print(max([len(a), len(b), len(c)]))
+print(min([len(a), len(b), len(c)]))
+
+names = ["Carol", "Albert", "Ben", "Donna"]
+print(" & ".join(sorted(names)))
+print("-".join(sorted(names)))
+
+names = ["Carol", "Albert", "Ben", "Donna"]
+names.append("Eugenia")
+print(sorted(names))
